54927711c3825b19a822f8c97b5a37e93dcb0c8a
EunJae <dldmswo1209@naver.com>
2022-09-20 23:20:03 +0900

    네비게이션 드로어 구현

--- app/src/main/java/com/dldmswo1209/portfolio/viewModel/MainViewModel.kt
+++ app/src/main/java/com/dldmswo1209/portfolio/viewModel/MainViewModel.kt
@@ -22,7 +22,11 @@ class MainViewModel(application: Application): AndroidViewModel(application) {
 
 
     // 모든 카드 리스트를 가져올 것을 repository 에게 요청하는 메소드
+    // 코루틴으로 작업이 진행되고 있는 상황에서 다른 화면으로 넘어간 경우에
+    // 코루틴을 중단시키는 작업을 해줘야하는데, 그 작업을 도와주는게 바로 viewModelScope 이다.
+    // 이녀석이 LifeCycle 을 인식해서 알아서 코루틴을 cancel 해준다
     fun getAllCard() = viewModelScope.launch(Dispatchers.IO) {
+        // Dispatchers.IO 로 명시를 안해주면 메인 스레드에서는 DB 에 접근할 수 없다는 오류 메세지가 뜨면서 앱이 죽는다
         _cardList.postValue(repository.getAllCard())
     }
     // 모든 채팅 리스트를 가져올 것을 repository 에게 요청하는 메소드
