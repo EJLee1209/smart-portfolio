2f7550e77797ee5cf63fe7ac697921c1a335d8c8
EunJae <dldmswo1209@naver.com>
2022-09-23 00:42:10 +0900

    카드 포트폴리오 드래그앤 드랍 기능 구현

--- app/src/main/java/com/dldmswo1209/portfolio/Fragment/CardFragment.kt
+++ app/src/main/java/com/dldmswo1209/portfolio/Fragment/CardFragment.kt
@@ -12,7 +12,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.activityViewModels
 import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.ItemTouchHelper
 import com.dldmswo1209.portfolio.MainActivity
 import com.dldmswo1209.portfolio.R
 import com.dldmswo1209.portfolio.WebViewActivity
@@ -20,11 +20,48 @@ import com.dldmswo1209.portfolio.adapter.CardListAdapter
 import com.dldmswo1209.portfolio.adapter.CardListAdapter.Companion.EDIT_CARD
 import com.dldmswo1209.portfolio.adapter.CardListAdapter.Companion.GO_HOMEPAGE
 import com.dldmswo1209.portfolio.databinding.FragmentCardBinding
+import com.dldmswo1209.portfolio.swipeHelper.ItemDragHelperCallback
 import com.dldmswo1209.portfolio.viewModel.MainViewModel
 
 class CardFragment : Fragment(R.layout.fragment_card) {
     private lateinit var binding : FragmentCardBinding
     private val viewModel: MainViewModel by activityViewModels()
+    val cardAdapter = CardListAdapter { cardEntity, type ->
+        // 어답터를 생성할 때 아이템 클릭 이벤트를 정의함
+        if(type == GO_HOMEPAGE) {
+            // 아이템 클릭시 다이얼로그를 띄워 웹을 띄울 방법(내부/외부)을 선택
+            val builder = AlertDialog.Builder(requireContext())
+            builder.setTitle("홈페이지 이동")
+                .setMessage("내부 웹뷰 또는 외부 웹 브라우저를 선택해주세요.")
+                .setPositiveButton("내부", DialogInterface.OnClickListener { dialog, id ->
+                    val intent = Intent(requireContext(), WebViewActivity::class.java)
+                    intent.putExtra("url", cardEntity.link)
+                    startActivity(intent)
+                })
+                .setNegativeButton("외부", DialogInterface.OnClickListener { dialog, id ->
+                    val intent = Intent(Intent.ACTION_VIEW, Uri.parse(cardEntity.link))
+                    startActivity(intent)
+                })
+            builder.show()
+        }else if(type == EDIT_CARD){
+            // 수정하기
+            val bottomSheet = AddPortfolioBottomSheet(cardEntity)
+            bottomSheet.show((activity as MainActivity).supportFragmentManager, bottomSheet.tag)
+        }else{
+            // 삭제하기
+            val builder = AlertDialog.Builder(requireContext())
+            builder.setTitle("포트폴리오 삭제")
+                .setMessage("삭제 하시겠습니까?")
+                .setPositiveButton("삭제", DialogInterface.OnClickListener { dialog, id ->
+                    viewModel.deleteCard(cardEntity)
+                    dialog.dismiss()
+                })
+                .setNegativeButton("취소", DialogInterface.OnClickListener { dialog, id ->
+                    dialog.dismiss()
+                })
+            builder.show()
+        }
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -34,42 +71,16 @@ class CardFragment : Fragment(R.layout.fragment_card) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding = FragmentCardBinding.bind(view)
-        val cardAdapter = CardListAdapter { cardEntity, type ->
-            // 어답터를 생성할 때 아이템 클릭 이벤트를 정의함
-            if(type == GO_HOMEPAGE) {
-                // 아이템 클릭시 다이얼로그를 띄워 웹을 띄울 방법(내부/외부)을 선택
-                val builder = AlertDialog.Builder(requireContext())
-                builder.setTitle("홈페이지 이동")
-                    .setMessage("내부 웹뷰 또는 외부 웹 브라우저를 선택해주세요.")
-                    .setPositiveButton("내부", DialogInterface.OnClickListener { dialog, id ->
-                        val intent = Intent(requireContext(), WebViewActivity::class.java)
-                        intent.putExtra("url", cardEntity.link)
-                        startActivity(intent)
-                    })
-                    .setNegativeButton("외부", DialogInterface.OnClickListener { dialog, id ->
-                        val intent = Intent(Intent.ACTION_VIEW, Uri.parse(cardEntity.link))
-                        startActivity(intent)
-                    })
-                builder.show()
-            }else if(type == EDIT_CARD){
-                // 수정하기
-                val bottomSheet = AddPortfolioBottomSheet(cardEntity)
-                bottomSheet.show((activity as MainActivity).supportFragmentManager, bottomSheet.tag)
-            }else{
-                // 삭제하기
-                val builder = AlertDialog.Builder(requireContext())
-                builder.setTitle("포트폴리오 삭제")
-                    .setMessage("삭제 하시겠습니까?")
-                    .setPositiveButton("삭제", DialogInterface.OnClickListener { dialog, id ->
-                        viewModel.deleteCard(cardEntity)
-                        dialog.dismiss()
-                    })
-                    .setNegativeButton("취소", DialogInterface.OnClickListener { dialog, id ->
-                        dialog.dismiss()
-                    })
-                builder.show()
+
+        val callback = ItemDragHelperCallback(cardAdapter)
+        val touchHelper = ItemTouchHelper(callback)
+        touchHelper.attachToRecyclerView(binding.cardRecyclerView)
+        cardAdapter.startDrag(object: CardListAdapter.OnStartDragListener{
+            override fun onStartDrag(viewHolder: CardListAdapter.ViewHolder) {
+                touchHelper.startDrag(viewHolder)
             }
-        }
+
+        })
 
         // 모든 카드리스트 데이터 요청
         viewModel.getAllCard()
