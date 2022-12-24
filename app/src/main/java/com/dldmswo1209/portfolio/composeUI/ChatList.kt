c94170975192ef2cb06f448e9946c755003cafd3
EJLee1209 <dldmswo1209@gmail.com>
2022-12-25 01:02:12 +0900

    코드 간소화

--- app/src/main/java/com/dldmswo1209/portfolio/composeUI/ChatList.kt
+++ app/src/main/java/com/dldmswo1209/portfolio/composeUI/ChatList.kt
@@ -1,12 +1,8 @@
 package com.dldmswo1209.portfolio_compose
 
-import android.provider.MediaStore.Audio.Radio
-import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
@@ -15,16 +11,11 @@ import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.window.Dialog
-import androidx.compose.ui.window.DialogProperties
 import com.dldmswo1209.portfolio.Model.Chat
 import com.dldmswo1209.portfolio.composeUI.AddChatDialog
 import com.dldmswo1209.portfolio.composeUI.DeleteDialogScreen
-import com.dldmswo1209.portfolio.composeUI.ModifyChatDialog
 import com.dldmswo1209.portfolio.viewModel.MainViewModel
 
 @Composable
